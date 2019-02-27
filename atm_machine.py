2c052bf2ecf3847b4b26f218dd1e14fd0f144a82
[saransekar] <[saran@5gindia.net]>
2019-02-27 10:28:15 -0300

    Removed unwanted space, refactored functions and added docstring

--- atm_machine.py
+++ atm_machine.py
@@ -1,77 +1,55 @@
 import re
-def get_input_user():
-	print("User Details")
-	global UserName,MobileNumber,EmailId,UserBalance
-	UserName = input("Enter the User Name:")
-	return UserName
-
-def get_input_mobile():
-
-	MobileNumber = input("Enter the Your Mobile Number:")
-	regex = "(0/91)?[7-9][0-9]{9}"
-	return MobileNumber
-	#UserBalance = int(input("Enter the amount:")) 
-
-def get_input_email():
-	EmailId = input("Enter the Your EmailId:")
-	return EmailId
-
-def valid_inputs():	
+def valid_inputs(pattern,message,error):
+	"""check Validation inputs in the function  
+	Parameters:
+		pattern(str): Search the pattern to compare input.
+		message(str): Message throw to input and display 
+		error(str): If the condition is false display error message
+	Returns(str):
+		get string inputs"""	
 	while True:
-		Name = get_input_user()
-		NameMatch = re.search("[a-zA-Z_][a-zA-Z0-9]*", Name)
-		if NameMatch:
+		UserInput = input(message)		 
+		if re.search(pattern, UserInput):
 			break
 		else:
-			print("Invalid UserName")
-
-	while True:
-		Mobile= get_input_mobile()
-		MobileMatch = re.search("(0/91)?[7-9][0-9]{9}",Mobile)
-		if MobileMatch:
-			break	
-		else:
-			print("Invalid Mobile Number")
-
-	while True:
-		Email = get_input_email()
-		EmailMatch = re.search(r"[^@]+@[^@]+\.[^@]+", Email)
-		if EmailMatch:
-			break
-		else:
-			print("Invalid EmailId Address")
-
-
-def instruct_account_list():	
-	print("Please select any one below given:\n1.Check Your Balance\n2.Debit Amount\n3.Credit Amount\n4.Profile Details\n5.Exit")
-
+			print(error)	
+	return UserInput
+def instruct_manage_account():
+	"""Display instruction manage account"""	
+	print("Please select any one below given:\n1.Check Your Balance\n2.Debit Amount")
+	print("3.Credit Amount\n4.Profile Details\n5.Exit")
 def get_money(Debit):
-	global UserBalance
-	UserBalance = UserBalance - Debit  
-	
+	"""Get money from account 
+	Parameters:
+		Debit(int): Balance is subtracted to Debit and get total money"""
+	global Balance
+	Balance = Balance - Debit  	
 def transfer_money(Credit):
-	global UserBalance
-	UserBalance = Credit + UserBalance 
-	
+	"""Transfer money in the same account
+	Parameters:
+		Credit(int): Balance is added to credit and get total money"""		
+	global Balance
+	Balance = Credit + Balance 	
 def manage_account():
+	"""Debit, Credit, Check balance and Profile details in managing account"""	
 	while True:
 		try:
 			Option = int(input("Enter Option:"))
 			if Option == 1:
-				print("Currently Your Available Balance:",UserBalance)
+				print("Currently Your Available Balance:",Balance)
 			elif Option == 2:
 				Debit = int(input("Enter the Debit Amount:"))
-				if UserBalance >= Debit:						
+				if Balance >= Debit:						
 					get_money(Debit)							
-					print(UserBalance)
+					print(Balance)
 				else:
 					print("No Available Balance")								
 			elif Option == 3:
 				Credit = int(input("Enter the Credit Amount:"))
 				transfer_money(Credit)		
-				print(UserBalance)
+				print(Balance)
 			elif Option == 4:
-				print("Profile Details\nUser Name : {}\nMobile Number : {}\nEmail Id : {}\nAvailable Balance : {}".format(UserName,MobileNumber,EmailId,UserBalance)) 				
+				print("Profile Details\nUserName: {}\nMobileNumber: {}\nEmailId: {}\nAvailable Balance: {}".format(UserName,MobileNumber,Email,Balance)) 				
 			elif Option == 5:
 				print("Your transcation cancel")
 				exit()
@@ -79,12 +57,15 @@ def manage_account():
 				print("Doesn't exist")
 		except (SyntaxError, ValueError):		
 			print("You didn't enter a number")	
-
 def main():
-	#get_input_user()
-	#get_input_mobile()
-	Inputs = valid_inputs()
-	AccountBalance = instruct_account_list()
-	Result = manage_account()
+	global UserName,MobileNumber,Email,Balance
+	UserName = valid_inputs("[a-zA-Z_][a-zA-Z0-9]*","Enter Your Name:","Invalid UserName")	
+	MobileNumber = valid_inputs("(0/91)?[7-9][0-9]{9}","Enter Your MobileNumber:",
+	"Invalid MobileNumber")
+	Email = valid_inputs(r"[^@]+@[^@]+\.[^@]+","Enter Your EmailId:","Invalid EmailId")
+	Balance = valid_inputs(r"[1234567890]\d{2}$","Enter the Amount:","Invalid Digits")
+	Balance = int(Balance)
+	instruct_manage_account()
+	manage_account()
 if __name__ == '__main__':
   main()  
